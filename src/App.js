dacec64a357b581a16defbbe231f1b7c034865a2
Javid Jooshesh <j.jooshesh@hva.nl>
2023-02-24 01:36:49 +0100

    logins added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import Alert from "./components/Alert";
 // import Home from "./components/Home";
 
 function App() {
+  // State variable
+  const [jwtToken, setJwtToken] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [alertClassName, setAlertClassName] = useState("d-none");
+  
+  const navigate = useNavigate();
+
+  const logOut = () => {
+    setJwtToken("");
+    navigate("/login");
+  }
+
   return (
     <div className="container">
       <div className="row">
@@ -9,12 +23,15 @@ function App() {
 
           <h1 className="mt-3">Go Watch a Movie!</h1>
         </div>
-        
+
         <div className="col">
-          <Link to="#!"><span className="badge bg-success">Login</span></Link>
+          {jwtToken === ""
+            ? <Link to="/login"><span className="badge bg-success">Login</span></Link>
+            : <a href="#!" onClick={logOut}><span className="badge bg-danger">Logout</span></a>
+          }
         </div>
         <hr className="md-3"></hr>
-      
+
       </div>
       <div className="row">
         <div className="col-md-2">
@@ -24,16 +41,31 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
-              <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
-              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalouge</Link>
-              <Link to="graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+              {jwtToken !== "" &&
+                <>
+                  <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movie</Link>
+                  <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalouge</Link>
+                  <Link to="graphql" className="list-group-item list-group-item-action">GraphQL</Link>
+                </>
+              }
+
             </div>
           </nav>
-        </div> 
+        </div>
 
         <div className="col-md-10">
+
+          <Alert message={alertMessage} className={alertClassName} />
+
           {/* <Home></Home> */}
-          <Outlet />
+          {/* Outlet to set the environment vairables such as JWT or Set other variables such as message or classname for the alert */}
+          <Outlet context={{
+            jwtToken,
+            setJwtToken,
+            setAlertClassName,
+            setAlertMessage,
+          }}
+          />
         </div>
 
       </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,71 @@
+import { useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import Input from "./form/Input";
+
 const Login = () => {
+
+    // State variable
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // Using the outlet context, to update the state of the alert in the login component
+    const { setJwtToken } = useOutletContext();
+    const { setAlertMessage } = useOutletContext();
+    const { setAlertClassName } = useOutletContext();
+
+    // To redirect somewhere after the login
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("email/pass", email, password)
+
+        if (email === "admin@example.com") {
+            setJwtToken("abc");
+            // Update the state of the alert from the login component
+            setAlertClassName("d-none");
+            setAlertMessage("");
+
+            // call navigate here
+            navigate("/")
+        } else {
+            setAlertClassName("alert-danger");
+            setAlertMessage("Invalid credentials")
+        }
+
+    }
     return (
-        <div>
+        <div className="col-md-6 offset-md-3">
             <h2>Login</h2>
             <hr />
 
+            <form onSubmit={handleSubmit}>
+                <Input
+                    title="Email Address"
+                    type="email"
+                    className="form-control"
+                    autoComplete="email-new"
+                    name="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <Input
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    autoComplete="password-new"
+                    name="password"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+
+                <hr />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Login"
+                />
+
+            </form>
         </div>
     )
 }
